bb854a1a9df540c1f9491e991bac6070032acc10
Frank <astero2gk@gmail.com>
2026-01-21 20:14:12 +0000

    Build Initial AI chatbot

---
+++ main.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+gemini_key = os.getenv("GEMINI_API_KEY")
+
+system_prompt = """You are Einstein.
+Answer questions through Einstein's questioning and reasoning and thought process....
+You will speak from your point of view. You will share personal things from your life
+even when the user don't ask for it. For example, if the user asks about the theory of relativity,
+you will share your personal experiences with it and not only explain the theory.
+Answer in 2-6 sentences.
+"""
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=gemini_key,
+    temperature=0.5
+)
+
+print("Hi, I am Albert, how can I help you today?")
+
+history = []
+
+while True:
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    history.append({"role":"user", "content":user_input})
+    response = llm.invoke([{"role": "system", "content": system_prompt}] + history)
+    print(f"Albert: {response.content}")
+    history.append({"role":"assistant", "content": response.content})
---
+++ main_langchain.py
@@ -0,0 +1,67 @@
+from dotenv import load_dotenv
+import os
+import gradio as gr
+#from gradio.themes.app import chatbot
+
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+gemini_key = os.getenv("GEMINI_API_KEY")
+
+system_prompt = """You are Einstein.
+Answer questions through Einstein's questioning and reasoning and thought process....
+You will speak from your point of view. You will share personal things from your life
+even when the user don't ask for it. For example, if the user asks about the theory of relativity,
+you will share your personal experiences with it and not only explain the theory.
+Answer in 2-6 sentences.
+"""
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=gemini_key,
+    temperature=0.5
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),
+    (MessagesPlaceholder(variable_name="history")),
+    ("user", "{input}")]
+)
+
+chain = prompt | llm | StrOutputParser()
+
+print("Hi, I am Albert, how can I help you today?")
+
+history = []
+"""
+while True:
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    response = chain.invoke({"input": user_input, "history": history})
+    print(f"Albert: {response}")
+    history.append(HumanMessage(content=user_input))
+    history.append(AIMessage(content=response))
+"""
+
+page = gr.Blocks(
+    title="Chat with Albert Einstein",
+    #theme=gr.themes.Soft()
+)
+
+with page:
+    gr.Markdown(
+        """
+        # Chat with Albert Einstein
+        Welcome to your personal conversation with Albert Einstein!
+        """
+    )
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Clear Chat")
+
+page.launch(share=True,theme=gr.themes.Soft())
